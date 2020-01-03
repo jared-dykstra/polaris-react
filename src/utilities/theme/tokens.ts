960c2e627a470d4864bc817751c4e74695142d38
Laura Aubin <laura.aubin@shopify.com>
2020-01-03 13:06:33 -0500

    Light on light, dark on dark (without inverse)

--- src/utilities/theme/tokens.ts
+++ src/utilities/theme/tokens.ts
@@ -10,6 +10,8 @@ const Shadow = {
     '-1px 0px 20px var(--p-shadow-from-ambient-light), 0px 1px 5px var(--p-shadow-from-direct-light)',
   modalShadow:
     '0px 6px 32px var(--p-shadow-from-ambient-light), 0px 1px 6px var(--p-shadow-from-direct-light)',
+  tooltipShadow:
+    '-1px 0px 20px var(--p-shadow-from-ambient-light), 0px 1px 5px var(--p-shadow-from-direct-light)',
 };
 
 const Overrides = {
